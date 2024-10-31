7af0a997e8632d1e978afa5755ae78be62ec889e
Ryosuke Kambara <rosery030517@gmail.com>
2024-10-31 13:10:37 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,13 +1,26 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2024 Ryosuke Kambara
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo ${1}行目が違うよん
+	echo ${1}行目が違うよ
 	res=1
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] ||ng "$LINENO"
-[ "$a" = 山田 ] ||ng "$LINENO"
 
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus_stdin)
+[ "${out}" = 15 ] || ng "$LINENO"
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus_stdin)
+[ "$?" = 1 ]      || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus_stdin)
+[ "$?"  = 1 ]     || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
+
+[ "${res}" = 0 ] && echo OK
 exit $res
