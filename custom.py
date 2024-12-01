b59808abe3095d5b9bfb1c0d53b36f7b22841c1b
Ryosuke Kambara <rosery030517@gmail.com>
2024-12-01 15:52:16 +0900

    Add a command

--- custom.py
+++ custom.py
@@ -7,7 +7,7 @@ def main():
     swapped_text = text.swapcase()
     reversed_text = text[::-1]
 
-    # 結果を表示
+# 結果を表示
     print(color + "変換された文字列:", swapped_text, "\033[0m")
     print("逆さ読み:", reversed_text)
 
